77768688cb2a19dcf61d5016ac3e93aa49be89e7
Sofiane <s-attilah@hotmail.com>
2020-05-29 23:31:28 +0200

    test

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Mentor = require("../model/Mentor");
+const Mentor = require("./Mentor");
 const nodemailer = require("nodemailer");
 const hbs = require('nodemailer-handlebars');
 const path = require("path");
