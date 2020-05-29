5595ac2cb6b6ffdc33cf20cf9f39794a02388566
Sofiane <s-attilah@hotmail.com>
2020-05-30 00:16:12 +0200

    test responsive on mobile

--- client/src/components/layout/SquadCard.js
+++ client/src/components/layout/SquadCard.js
@@ -6,14 +6,12 @@ import "../../css/squad-card.css"
 const SquadCard = props => {
     return (
         <>
-        <Card className="swiper-slide mb-4 mt-4 ml-3 mr-3">
+        <Card className="swiper-slide mb-5 mt-5 ml-3 mr-3">
             <Card.Img className="card-img" variant="top" src={props.img} />
             <Card.Title className="text-white p-2 position-absolute lead" style={{top: "0", right: "0"}}>{props.statut} <i className="fas fa-circle text-success"></i></Card.Title>
             <Card.Body className="position-absolute" style={{ bottom: "3%", width: "100%"}}>
-                <div className="d-flex justify-content-between">
-                    <Card.Title className="text-white pt-1 pb-1">{props.title}</Card.Title>
-                    <Card.Title className="text-white card-price pt-1 pb-1 pl-2 pr-2">{props.price}</Card.Title>     
-                </div>
+                <Card.Title className="text-white pt-1 pb-1">{props.title}</Card.Title>
+                <Card.Title className="text-white card-price pt-1 pb-1 pl-2 pr-2 w-50">{props.price}€ <span style={{fontSize: "15px"}}> / h</span></Card.Title>     
                 <Card.Text className="text-white text-truncate">{props.bio}</Card.Text>
                 <Button className="btn-squad-color" onClick={props.onClick} block>Participez au live</Button>
             </Card.Body>
