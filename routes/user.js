f9f5b124b792620591f48907ae49f104b53ccf53
Sofiane <s-attilah@hotmail.com>
2020-05-29 23:39:16 +0200

    test export Mentor.js model in user.js

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Mentor = require("./Mentor");
+const Mentor = require("../models/Mentor");
 const nodemailer = require("nodemailer");
 const hbs = require('nodemailer-handlebars');
 const path = require("path");
--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const mongoose = require("mongoose");
 const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 5000;
-const secure = require('ssl-express-www');
 const isProd = process.env.NODE_ENV === "production";
 const user = require('./routes/user');
 
@@ -14,7 +13,6 @@ mongoose
     .then(() => console.log("Mongo DB succesfully connected"))
     .catch(err => console.log(err))
 
-app.use(secure);
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use("/user", user);
